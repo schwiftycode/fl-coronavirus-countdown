87ae8d134f1e76480d5a003d795b8afabdba93e3
Taylor Michel <tmichel@tmichelpro.com>
2020-04-15 16:00:54 -0400

    Working on Ads

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,12 +1,8 @@
 import React, { useEffect } from 'react'
+import AdSense from 'react-adsense'
 import { Timer } from '../components'
 
 const Home = props => {
-
-    useEffect(_ => {
-        let adsbygoogle = window.adsbygoogle || []
-        adsbygoogle.push({});
-    }, [])
 
     return (
         <div className="Home Screen">
@@ -23,12 +19,11 @@ const Home = props => {
             {/* Ad Space */}
             <div className="ads-container">
 
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                <ins className="adsbygoogle"
-                    data-ad-client="ca-pub-9490678581055546"
-                    data-ad-slot="6385376263"
-                    data-ad-format="auto"
-                    data-full-width-responsive="true"></ins>
+                <AdSense.Google className="adsbygoogle"
+                    client="ca-pub-9490678581055546"
+                    slot="6385376263"
+                    format="fluid"
+                    responsive="true" />
 
             </div>
         </div>
